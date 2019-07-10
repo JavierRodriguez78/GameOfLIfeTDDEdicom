dbc7314f62329cfee3f482efd647f2cb5449635c
Xavi <xavi@geekshubs.com>
2019-07-10 17:51:50 +0200

    Implemented LessThanTwooNeighbors

--- src/test/java/com/javier/rodriguez/GameOfLifeTDD/GameOfLifeTest.java
+++ src/test/java/com/javier/rodriguez/GameOfLifeTDD/GameOfLifeTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 /**
  * Unit test for simple App.
@@ -19,6 +20,22 @@ public class GameOfLifeTest
 		
 		assertTrue(cell.isAlive());
 		
+	}
+	@Test
+	public void isDeadCell()
+	{
+		Cell cell = new Cell(false);
+		
+		assertFalse(cell.isAlive());
+		
+	}
+	
+	@Test
+	public void itShouldDeadIfLessThanTwooNeighbors()
+	{
+		GameOfLife gameOfLife = new GameOfLife();
+		boolean result = gameOfLife.getNextStatus(true,1);
+		assertFalse(result);
 	}
 	
 	
