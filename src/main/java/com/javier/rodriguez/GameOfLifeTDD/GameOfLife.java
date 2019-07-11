3f6edbefd6cd459707f35fd23155f0ef49a751aa
Xavi <xavi@geekshubs.com>
2019-07-11 09:00:35 +0200

    [BLU] Refactor magic numbers

--- src/main/java/com/javier/rodriguez/GameOfLifeTDD/GameOfLife.java
+++ src/main/java/com/javier/rodriguez/GameOfLifeTDD/GameOfLife.java
@@ -2,10 +2,13 @@ package com.javier.rodriguez.GameOfLifeTDD;
 
 public class GameOfLife {
 
+	private static final int OVERPOPULATION_THRESHOLD = 3;
+	private static final int UNDERPOPULATION_THRESHOLD = 2;
+
 	public boolean getNextStatus(boolean status, int neighbors)
 	{
-		if (neighbors<2) return false;
-		if  (neighbors>3) return false;
+		if (neighbors<UNDERPOPULATION_THRESHOLD) return false;
+		if  (neighbors>OVERPOPULATION_THRESHOLD) return false;
 		return true;
 	}
 	
