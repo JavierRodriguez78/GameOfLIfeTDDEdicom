2ecbd67d78b43e390d9276b4ac6deb1ffad496d9
Xavi <xavi@geekshubs.com>
2019-07-11 08:58:47 +0200

    [GREEN] Implemented itShouldDeadIfMoreThanThreeNeighbors to method

--- src/main/java/com/javier/rodriguez/GameOfLifeTDD/GameOfLife.java
+++ src/main/java/com/javier/rodriguez/GameOfLifeTDD/GameOfLife.java
@@ -5,6 +5,7 @@ public class GameOfLife {
 	public boolean getNextStatus(boolean status, int neighbors)
 	{
 		if (neighbors<2) return false;
+		if  (neighbors>3) return false;
 		return true;
 	}
 	
